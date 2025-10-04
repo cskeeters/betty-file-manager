5d1e283e4a12b9de5d90dc5a9e86a15ebea27209
Chad Skeeters <chad.skeeters@gmail.com>
2025-10-04 10:17:56 -0500

    Modifies file extension colorizer to look for . too.

--- view.go
+++ view.go
@@ -318,17 +318,17 @@ func (m *model) generateContent() string {
 			fileStyle = directory
 		} else if isSymDir(ct.absdir, f) {
 			fileStyle = symDirectory
-		} else if strings.HasSuffix(strings.ToLower(f.Name()),"xlsx") {
+		} else if strings.HasSuffix(strings.ToLower(f.Name()),".xlsx") {
 			fileStyle = excel
-		} else if strings.HasSuffix(strings.ToLower(f.Name()),"xls") {
+		} else if strings.HasSuffix(strings.ToLower(f.Name()),".xls") {
 			fileStyle = excel
-		} else if strings.HasSuffix(strings.ToLower(f.Name()),"xlsm") {
+		} else if strings.HasSuffix(strings.ToLower(f.Name()),".xlsm") {
 			fileStyle = excel
-		} else if strings.HasSuffix(strings.ToLower(f.Name()),"docx") {
+		} else if strings.HasSuffix(strings.ToLower(f.Name()),".docx") {
 			fileStyle = wordDoc
-		} else if strings.HasSuffix(strings.ToLower(f.Name()),"doc") {
+		} else if strings.HasSuffix(strings.ToLower(f.Name()),".doc") {
 			fileStyle = wordDoc
-		} else if strings.HasSuffix(strings.ToLower(f.Name()),"pdf") {
+		} else if strings.HasSuffix(strings.ToLower(f.Name()),".pdf") {
 			fileStyle = pdf
 		}
 
