6074ace39bbc30a7d9998291eccaf75f8386d6c0
Chad Skeeters <chad.skeeters@gmail.com>
2026-03-03 11:22:57 -0600

    Updates test_runner.sh

--- plugins/test_runner.sh
+++ plugins/test_runner.sh
@@ -7,14 +7,22 @@ die() {
 
 [[ -x "$1" ]] || die "pass in the plugin you want to test"
 
+# Testing with spaces in CWD and paths is a good idea.
+#
+# State file has:
+#   CWD of active Tab
+#   Path1
+#   Path2
+#   ...
 cat <<EOF > /tmp/BFM_STATE_TEST
-/tmp
-/tmp/foo
-/tmp/bar
+/tmp/my files
+/tmp/my files/foo bar1
+/tmp/my files/foo bar2
+
 EOF
 
-$1 /tmp/BFM_STATE_TEST /tmp/BFM_STATE_TEST
+$1 /tmp/BFM_STATE_TEST /tmp/BFM_CMD_TEST
 
 echo
 echo $1 output:
-cat /tmp/BFM_STATE_TEST
+cat /tmp/BFM_CMD_TEST
